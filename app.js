2fc65d88490f0fbaa82aeba1445b86bebbc8b2e2
Ewerton <ewerton.silva07@hotmail.com>
2024-01-16 14:14:41 -0300

    create a button to request a random pokemon from the API

--- app.js
+++ app.js
@@ -1,32 +1,38 @@
 const myReq = new XMLHttpRequest();
 const body = document.querySelector('body');
 
+const randomNumber = function () {
+    const num = Math.floor(Math.random() * 1017) + 1;
+    return num;
+}
 
-myReq.onload = function() {
-    const data = JSON.parse(this.responseText);
-    const { name, sprites } = data;
-    const newName = name[0].toUpperCase();
+addEventListener('click', () => {
+    myReq.onload = function() {
+        const data = JSON.parse(this.responseText);
+        const { name, sprites } = data;
+        const newName = name[0].toUpperCase();
+        
     
+        body.append(newName + name.slice(1))
+        function createNewImage(image) {
+            if(image !== null){
+                const img = document.createElement('img')
+                img.src = image;
+                body.append(img) 
+            }
+        }
 
-    body.append(newName + name.slice(1))
-    function createNewImage(image) {
-        const img = document.createElement('img')
-        img.src = image;
-        body.append(img) 
+        createNewImage(sprites.back_default);
+        createNewImage(sprites.front_default);
+        createNewImage(sprites.back_shiny);
+        createNewImage(sprites.front_shiny);
     }
-
-    createNewImage(sprites.back_default);
-    createNewImage(sprites.back_shiny);
-    createNewImage(sprites.front_default);
-    createNewImage(sprites.front_shiny);
-
-}
-
-myReq.onerror = function() {
-    console.log("ERROR!!!");
-    console.log(this)
-}
-
-
-myReq.open('GET', 'https://pokeapi.co/api/v2/pokemon/303');
-myReq.send();
+    
+    myReq.onerror = function() {
+        console.log("ERROR!!!");
+    }
+    
+    
+    myReq.open('GET', `https://pokeapi.co/api/v2/pokemon/${randomNumber()}`);
+    myReq.send();
+})
